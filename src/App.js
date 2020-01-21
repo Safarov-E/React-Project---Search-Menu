8afa6f8dd5cfe1327b562a106be2c840a4d0a0e5
Safarov <elkhan.safarov10@yandex.com>
2020-01-21 23:00:33 +0500

    Создание элемента поиска и католога с фильмами

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
 import React, { Component } from 'react';
+import './styles/main.css'
 
 class App extends Component {
   render() {
     return (
-      
+      <div className="container">
+        <div className="input-container">
+          <label>Search for a Movie</label>
+          <input type="text" />
+        </div>
+
+        <div className="movies-container">
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+
+          <div className="movie-container">
+            <div className="movie-image"></div>
+            <p className="movie-title">Dark Knight</p>
+            <p className="movie-year">2005</p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
